1066802fb00b5131add9ce8cb1e4ff0c1abeb3fb
Felix-CtrlV <kaungswan59@gmail.com>
2026-01-26 10:35:55 +0630

    improve

--- BackEnd/config/dbconfig.php
+++ BackEnd/config/dbconfig.php
@@ -1,15 +1,15 @@
 <?php
-$host = "mysql-16bab7fd-kaungswan59-86a3.h.aivencloud.com";
-$username = "avnadmin";
-$password = "AVNS_kEHRdCr_cgK14vicoWI";
-$port = 12525;
-$database = "Malltiverse";
+// $host = "mysql-16bab7fd-kaungswan59-86a3.h.aivencloud.com";
+// $username = "avnadmin";
+// $password = "AVNS_kEHRdCr_cgK14vicoWI";
+// $port = 12525;
+// $database = "Malltiverse";
 
-// $host = "localhost";
-// $username = "root";
-// $password = "kaung273";
-// $database = "malltiverse";
-// $port = 3306;
+$host = "localhost";
+$username = "root";
+$password = "kaung273";
+$database = "malltiverse";
+$port = 3306;
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
